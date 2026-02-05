d0e1a37f51ad715a829d2320951fd17f2199fce9
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-05 10:03:44 -0800

    P

--- src/main/java/com/learn/homework1/Main.java
+++ src/main/java/com/learn/homework1/Main.java
@@ -1,7 +1,13 @@
 package com.learn.homework1;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+
+        var context =
+                new AnnotationConfigApplicationContext();
+
         Parrot parrot = new Parrot();
     }
 }
