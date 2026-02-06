d328730fa7c45c2b597d56159fa9415bbfdfc078
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-06 05:47:27 -0800

    P

--- src/main/java/com/learn/homework3/ProjectConfig.java
+++ src/main/java/com/learn/homework3/ProjectConfig.java
@@ -7,5 +7,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "com.learn.homework3")
 public class ProjectConfig {
-
+    @Bean
+    public Parrot parrot1() {
+        Parrot p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+    @Bean
+    public Parrot parrot2() {
+        Parrot p = new Parrot();
+        p.setName("Miki");
+        return p;
+    }
+    @Bean
+    public Person person(Parrot parrot2) {
+        Person p = new Person();
+        p.setName("Ella");
+        p.setParrot(parrot2);
+        return p;
+    }
 }
